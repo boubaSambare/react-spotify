6ab1e0e8af0c54a390b6bcca1a10d31aedad2f76
boubaSambare <boubasambare@yahoo.it>
2019-11-29 11:00:11 +0100

    :construction:

---
+++ src/components/SpotifyApp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SpotifyApp extends Component {
+    render() {
+        return (
+            <div>
+                <p>main pages</p>
+            </div>
+        );
+    }
+}
+
+export default SpotifyApp;
