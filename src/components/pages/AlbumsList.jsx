a6e4e58df83826d691d19d85c5d9463d5feb3458
boubaSambare <boubasambare@yahoo.it>
2019-12-01 12:27:53 +0100

    :art:  spinner

--- src/components/pages/AlbumsList.jsx
+++ src/components/pages/AlbumsList.jsx
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from "react";
-import {  Col } from "reactstrap";
+import { Col, Spinner } from "reactstrap";
 import { getAlbums } from "../../helpers/getAlbums";
 import Album from "./Album";
 
 const AlbumsList = ({ search }) => {
   const [albums, setAlbums] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const fechtAlbums = async () => {
     let reponse = await getAlbums(search);
     setAlbums(reponse.data);
+    setIsLoading(false);
   };
   useEffect(() => {
     fechtAlbums();
   }, []);
   return (
     <>
+      {isLoading && (
+        <Spinner color="danger" style={{ width: "3rem", height: "3rem" }} />
+      )}
       {albums.map(item => (
         <Col md="3" key={item.id}>
           <Album albums={item} key={item.id} />
